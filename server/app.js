43a3c8f1392dcc4ac67cb7803fec357c63a15f3d
Ranjith <ranjith.velyetodi@klm.com>
2019-06-24 14:45:43 +0200

    feature: added server

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, '../dist/webauthn')))
+  .use(express.static(path.join(__dirname, '../dist')))
   .listen(PORT, () => {
     console.log(`Listening on ${ PORT }`);
     console.log(__dirname);
